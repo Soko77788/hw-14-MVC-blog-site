3e9ea98b63c2efa4d60abf18deabd36005d3c9ae
Soko77788 <NickSokolowski7@gmail.com>
2024-06-05 14:44:28 -0500

    seed data created

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,29 @@
+const sequelize = require('./config/connection');
+const { User, Post, Comment } = require('./models');
+
+const userData = require('./seeds/userData.json');
+const postData = require('./seeds/postData.json');
+const commentData = require('./seeds/commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Post.bulkCreate(postData, {
+    returning: true,
+  });
+
+  await Comment.bulkCreate(commentData, {
+    returning: true,
+  });
+
+  console.log('Database seeded successfully');
+  process.exit(0);
+};
+
+seedDatabase();
 
