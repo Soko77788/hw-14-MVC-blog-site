1d8c3c57c60fe49f5cb2296b18e3c24de5a2c13d
Soko77788 <NickSokolowski7@gmail.com>
2024-06-10 18:30:51 -0500

    connection.js updated for render upload

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,23 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'postgres'
-  }
-);
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL, {
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false
+      }
+    }
+  })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+      }
+    );
 
 module.exports = sequelize;
